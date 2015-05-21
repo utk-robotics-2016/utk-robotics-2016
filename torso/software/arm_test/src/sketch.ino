f7accaa8bb2da4d31fcab733eecfa76e5fa000f4
Cory Walker <cwalker32@gmail.com>
2015-05-21 02:55:19 +0000

    Arm calibration.

--- torso/software/arm_test/src/sketch.ino
+++ torso/software/arm_test/src/sketch.ino
@@ -1,13 +1,15 @@
 #include <Servo.h>
 
 Servo base;
-int BASECENTER = 90;
+int BASECENTER = 89; // more positive moves to the right
 Servo shoulder;
-int SHOULDERCENTER = 90;
+int SHOULDERCENTER = 95; // more positive moves backward
 Servo elbow;
-int ELBOWCENTER = 90;
+int ELBOWCENTER = 126; // more positive moves down
+int ELBOWUP = ELBOWCENTER-90; // more positive moves down
 Servo wrist;
-int WRISTCENTER = 90;
+int WRISTCENTER = 95; // more positive flexes up
+int WRISTDOWN = WRISTCENTER-82;
 Servo wristrotate;
 int WRISTROTATECENTER = 90;
 Servo suction;
@@ -20,9 +22,11 @@ void setup()
     shoulder.attach(5);
     shoulder.write(SHOULDERCENTER);
     elbow.attach(6);
-    elbow.write(ELBOWCENTER);
+    /*elbow.write(ELBOWCENTER);*/
+    elbow.write(ELBOWUP);
     wrist.attach(9);
     wrist.write(WRISTCENTER);
+    /*wrist.write(WRISTDOWN);*/
     wristrotate.attach(10);
     wristrotate.write(WRISTROTATECENTER);
     suction.attach(11);
