390d1471655498e14fb77921e63ab117d2f04821
Cory Walker <cwalker32@gmail.com>
2015-07-15 13:06:12 +0000

    Remove obsolete rules revisons. See the dedicated repository.

--- rules_revisions/compare.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#vimdiff <(tidy -iq $1/IEEE_SoutheastCon_2016_Hardware_Competition_R.html) <(tidy -iq $2/IEEE_SoutheastCon_2016_Hardware_Competition_R.html)
-vimdiff <(html2text $1/IEEE_SoutheastCon_2016_Hardware_Competition_R.html) <(html2text $2/IEEE_SoutheastCon_2016_Hardware_Competition_R.html)
