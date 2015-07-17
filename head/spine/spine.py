203efe7c442573e11536c1ee151d80af7be1d227
Cory Walker <cwalker32@gmail.com>
2015-07-17 00:37:44 +0000

    Modify spine to support both Arduinos. Will break some things.

--- head/spine/spine.py
+++ head/spine/spine.py
@@ -9,34 +9,40 @@ import serial
 # Project specific
 import mecanum
 
-#DEF_OSX_PORT= '/dev/tty.usbmodem12341'
-DEF_LINUX_PORT= '/dev/mega'
+DEF_PORTS = {
+    'mega': '/dev/mega',
+    'teensy': '/dev/teensy',
+}
 
 class Spine:
-    def __init__(self, port, baud=115200, t_out=1, delim='\n'):
-        self.ser = serial.Serial(port, baud, timeout=t_out)
+    def __init__(self, t_out=1, delim='\n'):
+        self.ser = {}
+        for devname, port in DEF_PORTS.iteritems():
+            self.ser[devname] = serial.Serial(port, 115200, timeout=t_out)
         self.delim = delim
         self.compass_tare = 0
         # Hook in SIGINT abort handler for clean aborts
         signal.signal(signal.SIGINT, self.signal_handler)
         # Read in enough bytes to clear the buffer
-        self.ser.read(1000)
+        for devname in self.ser.keys():
+            self.ser[devname].read(1000)
 
-    def send(self, command):
-        self.ser.write(command + self.delim)
-        echo = self.ser.readline()
+    def send(self, devname, command):
+        self.ser[devname].write(command + self.delim)
+        echo = self.ser[devname].readline()
         assert echo == '> ' + command + '\r\n'
-        response = self.ser.readline()
+        response = self.ser[devname].readline()
         # Be sure to chop off newline. We don't need it.
         return response[:-2]
 
     def ping(self):
-        response = self.send('ping')
-        assert response == 'ok'
+        for devname in self.ser.keys():
+            response = self.send(devname, 'ping')
+            assert response == 'ok'
 
-    def set_led(self, status):
+    def set_led(self, devname, status):
         command = 'le ' + ('on' if status else 'off')
-        response = self.send(command)
+        response = self.send(devname, command)
         assert response == 'ok'
 
     def move_for(self, delay, *args):
@@ -58,7 +64,7 @@ class Spine:
         w_id = [2,4,1,3][w_id-1]
         direction = {'fw': 'cw', 'rv': 'ccw'}[direction]
         command = 'go ' + str(w_id) + ' ' + str(speed) + ' ' + direction
-        response = self.send(command)
+        response = self.send('teensy', command)
         assert response == 'ok'
 
     def set_arm(self, rot):
@@ -66,18 +72,18 @@ class Spine:
         for r in rot:
             assert 0 <= r <= 180
         command = 'sa %d %d %d %d %d' % tuple(rot)
-        response = self.send(command)
+        response = self.send('mega', command)
         assert response == 'ok'
 
     def set_suction(self, suction):
         assert 0 <= suction <= 180
         command = 'ss %d' % suction
-        response = self.send(command)
+        response = self.send('mega', command)
         assert response == 'ok'
 
     def detach_servos(self):
         command = 'ds'
-        response = self.send(command)
+        response = self.send('mega', command)
         assert response == 'ok'
 
     def move(self, speed, direction, angular):
@@ -91,12 +97,14 @@ class Spine:
     def stop(self):
         self.move(0, 0, 0)
 
+    '''
     def set_servo(self, s_id, pos):
         assert s_id in ['A', 'B']
         assert 0 <= pos <= 255
         command = 'sv ' + s_id + ' ' + str(pos)
         response = self.send(command)
         assert response == 'ok'
+    '''
 
     def startup(self):
         # Make sure we can ping the torso
@@ -104,9 +112,11 @@ class Spine:
 
         # Flash LED for visual confirmation
         for i in range(3):
-            self.set_led(True)
+            for devname in self.ser.keys():
+                self.set_led(devname, True)
             time.sleep(0.05)
-            self.set_led(False)
+            for devname in self.ser.keys():
+                self.set_led(devname, False)
             # Don't waste time after the last flash
             if i != 2:
                 time.sleep(0.05)
@@ -118,5 +128,6 @@ class Spine:
         sys.exit(0)
 
     def close(self):
-        self.ser.close()
-        print 'Closed serial connection %s.' % self.ser.port
+        for devname in self.ser.keys():
+            self.ser[devname].close()
+            print 'Closed serial connection %s.' % self.ser[devname].port
