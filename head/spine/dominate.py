a368fe08c2257d43d173df099fbaf8592ad65cf5
Cory Walker <cwalker32@gmail.com>
2015-07-22 02:29:06 +0000

    Changes necessary for serial fix.

--- head/spine/dominate.py
+++ head/spine/dominate.py
@@ -5,13 +5,16 @@ import logging
 # Local modules
 from head.spine.spine import get_spine
 
+logging.basicConfig(format='%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s', level=logging.INFO, datefmt='%I:%M:%S')
+logger = logging.getLogger(__name__)
+
 with get_spine() as s:
     class Robot:
         def __init__(self):
             pass
 
         def start(self):
-            print "Done!"
+            logger.info("Done!")
 
     bot = Robot()
     bot.start()
