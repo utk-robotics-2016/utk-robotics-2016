b443221b87bc0feaccb4ec3e3552b46682b0caac
cmwslw <cwalker32@gmail.com>
2015-07-23 10:37:50 -0400

    Added circle.yml.

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-find . -not -path '*/optics/*' -not -path '*/new_optics/*' -not -path '*/.eggs/*' -name '*.py' | xargs flake8
+find . -not -path '*/optics/*' -not -path '*/new_optics/*' -not -path '*/.eggs/*' -not -name 'Vec3d.py' -name '*.py' | xargs flake8 --max-complexity 10
+#vulture --exclude="/new_optics/,/optics/,/Vec3d.py" .
