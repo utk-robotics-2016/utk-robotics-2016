ac0b90f8afc066b0f7d1cf99dd1b470d1f4ef138
Cory Walker <cwalker32@gmail.com>
2015-07-24 02:27:11 +0000

    Joyserver improvements.

---
+++ utils/joystick/joyserver.py
@@ -0,0 +1,37 @@
+import Pyro4
+
+from head.spine.core import get_spine
+from head.spine.arm import Arm
+
+
+with get_spine() as s:
+
+    arm = Arm(s)
+
+    class JoyServer(object):
+
+        def move(self, speed, direction, angular):
+            print 'move', speed, direction, angular
+            s.move(speed, direction, angular)
+
+        def arm_set_pos(self, cuppos, wrist, wristrotate):
+            print 'arm_set_pos', cuppos, wrist, wristrotate
+            arm.set_pos(cuppos, wrist, wristrotate)
+
+        def arm_park(self, seconds):
+            print 'arm_park', seconds
+            arm.park(seconds)
+
+        def set_suction(self, suction):
+            print 'suction', suction
+            s.set_suction(suction)
+
+
+    daemon = Pyro4.Daemon(host='0.0.0.0', port=9091)
+    uri = daemon.register(JoyServer())
+
+    #print "The daemon runs on port:", daemon.port
+    print "The object's uri is:", uri
+    print "The object's remote uri is:", str(uri).replace('0.0.0.0', 'ieeebeagle.nomads.utk.edu')
+
+    daemon.requestLoop()
