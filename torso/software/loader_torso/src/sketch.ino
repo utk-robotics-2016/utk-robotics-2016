beb2d459b2ba49f7634c23e59aca561a53d214fa
Cory Walker <cwalker32@gmail.com>
2015-07-31 00:15:52 +0000

    Added torso commands for the motor driver shield.

--- torso/software/loader_torso/src/sketch.ino
+++ torso/software/loader_torso/src/sketch.ino
@@ -8,6 +8,22 @@ int numArgs = 0;
 // Pin definitions
 const char LED = 13;
 
+// For motor driver:
+#define BRAKEVCC 0
+#define FW   1
+#define BW  2
+#define BRAKEGND 3
+#define CS_THRESHOLD 100
+
+/*  VNH2SP30 pin definitions
+ xxx[0] controls '1' outputs
+ xxx[1] controls '2' outputs */
+int inApin[2] = {7, 4};  // INA: Clockwise input
+int inBpin[2] = {8, 9}; // INB: Counter-clockwise input
+int pwmpin[2] = {5, 6}; // PWM input
+int cspin[2] = {2, 3}; // CS: Current sense ANALOG input
+int enpin[2] = {0, 1}; // EN: Status of switches output (Analog pin)
+
 void setup() {
     // Init LED pin
     pinMode(LED, OUTPUT);
@@ -17,6 +33,66 @@ void setup() {
 
     // Display ready LED
     digitalWrite(LED,HIGH);
+
+    // Initialize digital pins as outputs
+    for (int i=0; i<2; i++)
+    {
+        pinMode(inApin[i], OUTPUT);
+        pinMode(inBpin[i], OUTPUT);
+        pinMode(pwmpin[i], OUTPUT);
+    }
+    // Initialize braked
+    for (int i=0; i<2; i++)
+    {
+        digitalWrite(inApin[i], LOW);
+        digitalWrite(inBpin[i], LOW);
+    }
+}
+
+void motorOff(int motor)
+{
+    digitalWrite(inApin[motor], LOW);
+    digitalWrite(inBpin[motor], LOW);
+    analogWrite(pwmpin[motor], 0);
+}
+
+/* motorGo() will set a motor going in a specific direction
+ the motor will continue going in that direction, at that speed
+ until told to do otherwise.
+ 
+ motor: this should be either 0 or 1, will selet which of the two
+ motors to be controlled
+ 
+ direct: Should be between 0 and 3, with the following result
+ 0: Brake to VCC
+ 1: Clockwise
+ 2: CounterClockwise
+ 3: Brake to GND
+ 
+ pwm: should be a value between ? and 1023, higher the number, the faster
+ it'll go
+ */
+void motorGo(uint8_t motor, uint8_t direct, uint8_t pwm)
+{
+  if (motor <= 1)
+  {
+    if (direct <=4)
+    {
+      // Set inA[motor]
+      if (direct <=1)
+        digitalWrite(inApin[motor], HIGH);
+      else
+        digitalWrite(inApin[motor], LOW);
+
+      // Set inB[motor]
+      if ((direct==0)||(direct==2))
+        digitalWrite(inBpin[motor], HIGH);
+      else
+        digitalWrite(inBpin[motor], LOW);
+
+      analogWrite(pwmpin[motor], pwm);
+    }
+  }
 }
 
 /* The loop is set up in two parts. First the Arduino does the work it needs to
@@ -124,6 +200,31 @@ void parseAndExecuteCommand(String command) {
             Serial.println(ledState);
         } else {
             Serial.println("error: usage - 'rl'");
+        }
+    }
+    else if(args[0].equals(String("mod"))) { // motor drive
+        if(numArgs == 4) {
+            int speed = args[2].toInt();
+            int mot = args[1].toInt();
+            int dir = FW;
+
+            if(args[3].equals(String("bw"))) {
+                dir = BW;
+            }
+
+            motorGo(mot, dir, speed);
+            Serial.println("ok");
+        } else {
+            Serial.println("error: usage - 'mod [0/1] [speed] [fw/bw]'");
+        }
+    }
+    else if(args[0].equals(String("mos"))) { // motor stop
+        if(numArgs == 2) {
+            int mot = args[1].toInt();
+            motorOff(mot);
+            Serial.println("ok");
+        } else {
+            Serial.println("error: usage - 'mos [0/1]'");
         }
     }
     else {
