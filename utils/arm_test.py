0dcf6f13416d1925ed9ef1f1452e18e8a5c9da9d
Cory Walker <cwalker32@gmail.com>
2015-09-04 16:43:18 +0000

    Arm test progress.

--- utils/arm_test.py
+++ utils/arm_test.py
@@ -11,6 +11,12 @@ fmt = '%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=fmt, level=logging.INFO, datefmt='%I:%M:%S')
 logger = logging.getLogger(__name__)
 
+SPEED = 1
+FULL_BLOCK_FORWARD = 11
+HEIGHT_TO_CLEAR_LOADER = 12
+RIGHT_DROP_XY = (16,0)
+BOTTOM_LEVEL_HEIGHT = -1 # height of the top surface of the bottom level
+
 with get_spine() as s:
     with get_arm(s) as arm:
         class Robot:
@@ -18,25 +24,24 @@ with get_spine() as s:
             def __init__(self):
                 pass
 
-            def start(self):
-                # raise ValueError()
-                # s.move(1, 180, 0)
-                arm.move_to(Vec3d(-6, 10, 3), 0, 180)
-                # time.sleep(2)
+            def pick_block(self):
+                # lateral, forward, height
+                arm.move_to(Vec3d(-6, FULL_BLOCK_FORWARD, BOTTOM_LEVEL_HEIGHT+4), 0, 180, SPEED)
                 s.set_suction(True)
-                arm.move_to(Vec3d(-6, 10, -2), 0, 180)
-                time.sleep(2)
-                arm.move_to(Vec3d(-6, 10, 3), 0, 180)
-                arm.move_to(Vec3d(-14, 5, 3), 0, 180)
+                arm.move_to(Vec3d(-6, FULL_BLOCK_FORWARD, BOTTOM_LEVEL_HEIGHT-1), 0, 180, SPEED)
+                time.sleep(0.5)
+                arm.move_to(Vec3d(-6, FULL_BLOCK_FORWARD, HEIGHT_TO_CLEAR_LOADER), 0, 180, SPEED)
+
+            def drop_block_right(self):
+                arm.move_to(Vec3d(RIGHT_DROP_XY[0], RIGHT_DROP_XY[1], HEIGHT_TO_CLEAR_LOADER), 0, 180, SPEED)
                 s.set_suction(False)
-                # arm.move_to(Vec3d(0, 10, 1.5), 0, 180)
-                # time.sleep(2)
-                # arm.move_to(Vec3d(0, 10, 0), 0, 180)
-                # time.sleep(2)
-                # arm.move_to(Vec3d(0, 10, 1.5), 0, 180)
-                # time.sleep(2)
-                # arm.move_to(Vec3d(0, 10, 3), 0, 180)
-                # time.sleep(2)
+                s.set_release_suction(True)
+                time.sleep(0.5)
+                s.set_release_suction(False)
+
+            def start(self):
+                self.pick_block()
+                self.drop_block_right()
                 logger.info("Done!")
 
         bot = Robot()
