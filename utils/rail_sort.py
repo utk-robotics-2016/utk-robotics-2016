40d2a57ea77d3c01c5ba9d8bedac9556f1cf9a46
Cory Walker <cwalker32@gmail.com>
2015-09-11 19:52:01 +0000

    Closer to both-layer sorting.

--- utils/rail_sort.py
+++ utils/rail_sort.py
@@ -25,48 +25,67 @@ with get_spine() as s:
                 elif method == 'deadreckon':
                     if currzone == 3 and destzone == 0:
                         # Bump up against barge
-                        s.move_for(5, 0.8, 0, 0.1)
+                        s.move_for(5, 0.8, 0, 0.08)
                         # Bump against railroad
                         s.move_for(1, 0.6, -70, 0)
                         # Move away from railroad
                         s.move_for(1, 0.6, 70, 0)
                     elif currzone != -1:
+                        # Last argument is rotational mvmt toward the railroad
+                        # To account for mecanum drift
                         s.move_for(2, 0.6, -180, -0.09)
                 else:
                     raise ValueError
 
-            def detect_blocks(self):
+            def detect_blocks(self, level):
                 blocks = []
-                blocks.append([{'color': 'blue', 'type': 'full'}])
-                blocks.append([{'color': 'red', 'type': 'full'}])
-                blocks.append([{'color': 'yellow', 'type': 'full'}])
-                blocks.append([{'color': 'green', 'type': 'full'}])
-                blocks.append([{'color': 'red', 'type': 'full'}])
-                blocks.append([{'color': 'blue', 'type': 'full'}])
-                blocks.append([{'color': 'green', 'type': 'full'}])
-                blocks.append([{'color': 'green', 'type': 'full'}])
+                if level == 'bottom':
+                    # Far right
+                    blocks.append([{'color': 'blue', 'type': 'full'}])
+                    blocks.append([{'color': 'red', 'type': 'full'}])
+                    blocks.append([{'color': 'yellow', 'type': 'full'}])
+                    blocks.append([{'color': 'green', 'type': 'full'}])
+                    blocks.append([{'color': 'red', 'type': 'full'}])
+                    blocks.append([{'color': 'blue', 'type': 'full'}])
+                    blocks.append([{'color': 'green', 'type': 'full'}])
+                    blocks.append([{'color': 'green', 'type': 'full'}])
+                    # Far left
+                elif level == 'top':
+                    # Far right
+                    blocks.append([{'color': 'green', 'type': 'full'}])
+                    blocks.append([{'color': 'yellow', 'type': 'full'}])
+                    blocks.append([{'color': 'yellow', 'type': 'full'}])
+                    blocks.append([{'color': 'blue', 'type': 'full'}])
+                    blocks.append([{'color': 'blue', 'type': 'full'}])
+                    blocks.append([{'color': 'red', 'type': 'full'}])
+                    blocks.append([{'color': 'yellow', 'type': 'full'}])
+                    blocks.append([{'color': 'red', 'type': 'full'}])
+                    # Far left
+                else:
+                    raise ValueError
                 return blocks
 
             def get_zone_color(self, zid):
                 return ['yellow', 'blue', 'red', 'green'][zid]
 
             def start(self):
-                blocks = self.detect_blocks()
-                for b in blocks:
-                    assert b[0]['type'] == 'full'
                 lastzid = -1
-                for zid in range(4):
-                    self.move_to_zone(lastzid, zid)
-                    color = self.get_zone_color(zid)
-                    indices = []
-                    for i, b in enumerate(blocks):
-                        if b[0]['color'] == color:
-                            indices.append(i)
-                    logging.info('%s blocks at %s.' % (color, indices))
-                    for i in indices:
-                        self.bp.pick_block(i, 'bottom', 'full')
-                        self.bp.drop_block_right(rail=True)
-                    lastzid = zid
+                for level in ['top', 'bottom']:
+                    blocks = self.detect_blocks(level)
+                    for b in blocks:
+                        assert b[0]['type'] == 'full'
+                    for zid in range(4):
+                        self.move_to_zone(lastzid, zid)
+                        color = self.get_zone_color(zid)
+                        indices = []
+                        for i, b in enumerate(blocks):
+                            if b[0]['color'] == color:
+                                indices.append(i)
+                        logging.info('%s blocks at %s.' % (color, indices))
+                        for i in indices:
+                            self.bp.pick_block(i, level, 'full')
+                            self.bp.drop_block_right(rail=True)
+                        lastzid = zid
 
         bot = Robot()
         bot.start()
