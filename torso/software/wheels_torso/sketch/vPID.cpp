4ee90e6f24e41ee86b6dc8b085a02de431643789
amessing <amessing@utk.edu>
2015-09-15 13:33:56 -0400

    Set up localized vPID and I2CEncoder files so that modifications are easier and they can be transferred via git. Added I2CEncoder getVelocity function (as the getSpeed does not determine direction). Also lowered the time determined to be 0 to 1 second from 4 (This can lowered further if deemed necessary). Fixed a bug in the vPID source file. Also changed the output limits for the pid.

--- torso/software/wheels_torso/sketch/vPID.cpp
+++ torso/software/wheels_torso/sketch/vPID.cpp
@@ -18,63 +18,63 @@
  *    The parameters specified here are those for for which we can't set up 
  *    reliable defaults, so we need to have the user set them.
  ***************************************************************************/
-vPID::vPID(double* Input, double* Output, double* Setpoint,
-        double Kp, double Ki, double Kd, int ControllerDirection)
-{
-	
+ vPID::vPID(double* Input, double* Output, double* Setpoint,
+  double Kp, double Ki, double Kd, int ControllerDirection)
+ {
+
     myOutput = Output;
     myInput = Input;
     mySetpoint = Setpoint;
-	inAuto = false;
-	
-	PID::SetOutputLimits(0, 255);				//default output limit corresponds to 
+    inAuto = false;
+
+	  vPID::SetOutputLimits(0, 255);				//default output limit corresponds to 
 												//the arduino pwm limits
 
     SampleTime = 100;							//default Controller Sample Time is 0.1 seconds
 
-    PID::SetControllerDirection(ControllerDirection);
-    PID::SetTunings(Kp, Ki, Kd);
+    vPID::SetControllerDirection(ControllerDirection);
+    vPID::SetTunings(Kp, Ki, Kd);
 
     lastTime = millis()-SampleTime;				
-}
- 
- 
+  }
+
+
 /* Compute() **********************************************************************
  *     This, as they say, is where the magic happens.  this function should be called
  *   every time "void loop()" executes.  the function will decide for itself whether a new
  *   pid Output needs to be computed.  returns true when the output is computed,
  *   false when nothing has been done.
  **********************************************************************************/ 
-bool vPID::Compute()
-{
+ bool vPID::Compute()
+ {
    if(!inAuto) return false;
    unsigned long now = millis();
    unsigned long timeChange = (now - lastTime);
    if(timeChange>=SampleTime)
    {
       /*Compute all the working error variables*/
-	  double input = *myInput;
-      double error = *mySetpoint - input;
-      ITerm+= (ki * error);
-      if(ITerm > outMax) ITerm= outMax;
-      else if(ITerm < outMin) ITerm= outMin;
-      double dInput = (input - lastInput);
- 
+     double input = *myInput;
+     double error = *mySetpoint - input;
+     ITerm+= (ki * error);
+     if(ITerm > outMax) ITerm= outMax;
+     else if(ITerm < outMin) ITerm= outMin;
+     double dInput = (input - lastInput);
+
       /*Compute PID Output*/
-      double output = lastOutput + kp * error + ITerm- kd * dInput;
-      
-	  if(output > outMax) output = outMax;
-      else if(output < outMin) output = outMin;
-	  *myOutput = output;
-	  
+     double output = lastOutput + kp * error + ITerm- kd * dInput;
+
+     if(output > outMax) output = outMax;
+     else if(output < outMin) output = outMin;
+     *myOutput = output;
+
       /*Remember some variables for next time*/
-      lastInput = input;
-      lastTime = now;
-      lastOutput = output;
-	  return true;
+     lastInput = input;
+     lastTime = now;
+     lastOutput = output;
+     return true;
    }
    else return false;
-}
+ }
 
 
 /* SetTunings(...)*************************************************************
@@ -82,40 +82,40 @@ bool vPID::Compute()
  * it's called automatically from the constructor, but tunings can also
  * be adjusted on the fly during normal operation
  ******************************************************************************/ 
-void vPID::SetTunings(double Kp, double Ki, double Kd)
-{
+ void vPID::SetTunings(double Kp, double Ki, double Kd)
+ {
    if (Kp<0 || Ki<0 || Kd<0) return;
- 
+
    dispKp = Kp; dispKi = Ki; dispKd = Kd;
    
    double SampleTimeInSec = ((double)SampleTime)/1000;  
    kp = Kp;
    ki = Ki * SampleTimeInSec;
    kd = Kd / SampleTimeInSec;
- 
-  if(controllerDirection ==REVERSE)
+
+   if(controllerDirection ==REVERSE)
    {
-      kp = (0 - kp);
-      ki = (0 - ki);
-      kd = (0 - kd);
-   }
+    kp = (0 - kp);
+    ki = (0 - ki);
+    kd = (0 - kd);
+  }
 }
-  
+
 /* SetSampleTime(...) *********************************************************
  * sets the period, in Milliseconds, at which the calculation is performed	
  ******************************************************************************/
-void vPID::SetSampleTime(int NewSampleTime)
-{
+ void vPID::SetSampleTime(int NewSampleTime)
+ {
    if (NewSampleTime > 0)
    {
-      double ratio  = (double)NewSampleTime
-                      / (double)SampleTime;
-      ki *= ratio;
-      kd /= ratio;
-      SampleTime = (unsigned long)NewSampleTime;
-   }
+    double ratio  = (double)NewSampleTime
+    / (double)SampleTime;
+    ki *= ratio;
+    kd /= ratio;
+    SampleTime = (unsigned long)NewSampleTime;
+  }
 }
- 
+
 /* SetOutputLimits(...)****************************************************
  *     This function will be used far more often than SetInputLimits.  while
  *  the input to the controller will generally be in the 0-1023 range (which is
@@ -124,20 +124,20 @@ void vPID::SetSampleTime(int NewSampleTime)
  *  want to clamp it from 0-125.  who knows.  at any rate, that can all be done
  *  here.
  **************************************************************************/
-void vPID::SetOutputLimits(double Min, double Max)
-{
+ void vPID::SetOutputLimits(double Min, double Max)
+ {
    if(Min >= Max) return;
    outMin = Min;
    outMax = Max;
- 
+
    if(inAuto)
    {
-	   if(*myOutput > outMax) *myOutput = outMax;
-	   else if(*myOutput < outMin) *myOutput = outMin;
-	 
-	   if(ITerm > outMax) ITerm= outMax;
-	   else if(ITerm < outMin) ITerm= outMin;
-   }
+    if(*myOutput > outMax) *myOutput = outMax;
+    else if(*myOutput < outMin) *myOutput = outMin;
+
+    if(ITerm > outMax) ITerm= outMax;
+    else if(ITerm < outMin) ITerm= outMin;
+  }
 }
 
 /* SetMode(...)****************************************************************
@@ -145,27 +145,27 @@ void vPID::SetOutputLimits(double Min, double Max)
  * when the transition from manual to auto occurs, the controller is
  * automatically initialized
  ******************************************************************************/ 
-void vPID::SetMode(int Mode)
-{
-    bool newAuto = (Mode == AUTOMATIC);
-    if(newAuto == !inAuto)
+ void vPID::SetMode(int Mode)
+ {
+  bool newAuto = (Mode == AUTOMATIC);
+  if(newAuto == !inAuto)
     {  /*we just went from manual to auto*/
-        PID::Initialize();
-    }
-    inAuto = newAuto;
+    vPID::Initialize();
 }
- 
+inAuto = newAuto;
+}
+
 /* Initialize()****************************************************************
  *	does all the things that need to happen to ensure a bumpless transfer
  *  from manual to automatic mode.
  ******************************************************************************/ 
-void vPID::Initialize()
-{
+ void vPID::Initialize()
+ {
    ITerm = *myOutput;
    lastInput = *myInput;
    if(ITerm > outMax) ITerm = outMax;
    else if(ITerm < outMin) ITerm = outMin;
-}
+ }
 
 /* SetControllerDirection(...)*************************************************
  * The PID will either be connected to a DIRECT acting process (+Output leads 
@@ -173,25 +173,25 @@ void vPID::Initialize()
  * know which one, because otherwise we may increase the output when we should
  * be decreasing.  This is called from the constructor.
  ******************************************************************************/
-void vPID::SetControllerDirection(int Direction)
-{
+ void vPID::SetControllerDirection(int Direction)
+ {
    if(inAuto && Direction !=controllerDirection)
    {
-	  kp = (0 - kp);
-      ki = (0 - ki);
-      kd = (0 - kd);
+     kp = (0 - kp);
+     ki = (0 - ki);
+     kd = (0 - kd);
    }   
    controllerDirection = Direction;
-}
+ }
 
 /* Status Funcions*************************************************************
  * Just because you set the Kp=-1 doesn't mean it actually happened.  these
  * functions query the internal state of the PID.  they're here for display 
  * purposes.  this are the functions the PID Front-end uses for example
  ******************************************************************************/
-double vPID::GetKp(){ return  dispKp; }
-double vPID::GetKi(){ return  dispKi;}
-double vPID::GetKd(){ return  dispKd;}
-int vPID::GetMode(){ return  inAuto ? AUTOMATIC : MANUAL;}
-int vPID::GetDirection(){ return controllerDirection;}
+ double vPID::GetKp(){ return  dispKp; }
+ double vPID::GetKi(){ return  dispKi;}
+ double vPID::GetKd(){ return  dispKd;}
+ int vPID::GetMode(){ return  inAuto ? AUTOMATIC : MANUAL;}
+ int vPID::GetDirection(){ return controllerDirection;}
 
