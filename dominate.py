0ad91a6b58cbcf365fb9fbf203b46eeef1db949d
Andrew <amessing@utk.edu>
2015-09-29 20:23:22 +0000

    keyframe function added from utils. robot can now almost get to sea zone

--- dominate.py
+++ dominate.py
@@ -1,7 +1,7 @@
 # Python modules
 import time
 import logging
-
+import operator
 # Local modules
 from head.spine.core import get_spine
 from head.spine.loader import Loader
@@ -9,6 +9,33 @@ from head.spine.loader import Loader
 fmt = '%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=fmt, level=logging.DEBUG, datefmt='%I:%M:%S')
 logger = logging.getLogger(__name__)
+
+def keyframe(f, middleargs, seconds, startargs, endargs):
+    ramptime = 1
+    if seconds < ramptime * 2:
+        ramptime = float(seconds) / 2
+    start_time = time.time()
+    start_difference = map(operator.sub, middleargs, startargs)
+    end_difference = map(operator.sub, endargs, middleargs)
+    curr_time = time.time()
+    iters = 0
+    while (curr_time - start_time) < seconds:
+        elapsed = curr_time - start_time
+        if elapsed < ramptime:
+            fraction = elapsed / ramptime
+            toadd = [v * fraction for v in start_difference]
+            currargs = map(operator.add, startargs, toadd)
+        elif elapsed > (seconds - ramptime):
+            fraction = (elapsed - (seconds - ramptime)) / ramptime
+            toadd = [v * fraction for v in end_difference]
+            currargs = map(operator.add, middleargs, toadd)
+        else:
+            currargs = middleargs
+        f(*currargs)
+        curr_time = time.time()
+        iters += 1
+    logger.info("Keyframe iterations: %d", iters)
+    f(*endargs)
 
 with get_spine() as s:
     class Robot:
@@ -39,8 +66,9 @@ with get_spine() as s:
             s.move(speed, self.dir_mod * dir, angle)
 
         def move_to_corner(self):
-            self.move_pid(1, 0, 0)
-            time.sleep(5)
+            keyframe(self.move_pid,(1,0,0),6,(0,0,0),(1,0,0))    
+            #self.move_pid(1, 0, 0)
+            #time.sleep(5)
             self.move(1, 75, 0)
             time.sleep(.25)
 
@@ -56,6 +84,12 @@ with get_spine() as s:
             logger.info("Found white")
             self.white_square = self.white_square + 1
             s.stop()
+        
+        def strafe_until_black(self):
+            self.move_pid(1,-90,0)
+            while s.read_line_sensors()['left'] < self.qtr_threshold:
+                time.sleep(0.01)
+            s.stop()
 
         def wait_until_arm_limit_pressed(self):
             while not s.read_arm_limit():
@@ -66,13 +100,17 @@ with get_spine() as s:
             logger.info("Done!")
 
     bot = Robot()
-    bot.wait_until_arm_limit_pressed()
+    #    bot.wait_until_arm_limit_pressed()
     time.sleep(0.5)
     bot.start()
-    while bot.white_square < 3:
+    
+    bot.move_pid(1, -135, -.1)
+    time.sleep(0.375)
+    
+    for i in range(2):
         bot.strafe_until_white()
-        logger.info("Found the white square #" + str(bot.white_square))
-        time.sleep(1)
-        if bot.white_square < 3:
-            bot.move(1, -78, 0)
-            time.sleep(3)
+        bot.strafe_until_black()
+    bot.strafe_until_white()
+
+    keyframe(bot.move_pid,(1,-180,0),4.5,(0,-180,0),(0,-180,0))
+    
