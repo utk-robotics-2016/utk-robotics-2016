69be95228e7ad3e8331233a0f914dc8464aeeed1
amessing <amessing@utk.edu>
2015-11-11 11:17:18 -0500

    Added strafe_at_distance

--- head/spine/ultrasonic.py
+++ head/spine/ultrasonic.py
@@ -1,6 +1,63 @@
+import time
 import math
+from control import keyframe
 
 
+# TO BE TESTED
+def strafe_at_distance(s, dist, unit, dir, total_time, rampUp=1.0, rampDown=1.0):
+    assert dir in ['left', 'right']
+    assert unit in ['inch', 'cm']
+
+    middle_time = total_time - rampUp - rampDown
+    assert middle_time > 0
+
+    unit_mult = 1.0
+    if unit == 'cm':
+        unit_mult = 2.54
+
+    if dir == 'left':
+        angle = -90
+    else:
+        angle = 90
+
+    if rampUp:
+        keyframe(s.move_pid, (0, angle, 0), (1.0, angle, 0), rampUp)
+    else:
+        s.move_pid(1.0, angle, 0)
+
+    last_angle = angle
+
+    start_time = time.time()
+    current_time = start_time
+    while current_time - start_time < middle_time:
+        current_dist = (s.read_ultrasonics('front_left', unit) + s.read_ultrasonics('front_right', unit)) / 2.0
+        delta_dist = dist - current_dist
+        if dir == 'left':
+            if delta_dist > 1.0 * unit_mult:
+                current_angle = angle - 10
+            elif delta_dist < 1.0 * unit_mult:
+                current_angle = angle + 10
+            else:
+                current_angle = angle
+        else:
+            if delta_dist > 1.0 * unit_mult:
+                current_angle = angle + 10
+            elif delta_dist < 1.0 * unit_mult:
+                current_angle = angle - 10
+            else:
+                current_angle = angle
+
+        if current_angle != last_angle:
+            s.move_pid(1.0, current_angle, 0.0)
+            last_angle = current_angle
+
+    if rampDown:
+        keyframe(s.move_pid, (0.0, angle, 0), (0.0, angle, 0), rampDown)
+
+    s.stop()
+
+
+# TO BE TESTED
 def ultrasonic_go_to_position(s, front, left, right, unit='inch', p=.1):
     assert unit in ['inch', 'cm']
 
@@ -30,6 +87,7 @@ def ultrasonic_go_to_position(s, front, left, right, unit='inch', p=.1):
             speed = 1.0
 
         s.move_pid(speed, dir, 0)
+        last_speed = speed
 
         while(abs(delta) > threshold):
             if(side == 'front'):
@@ -49,7 +107,9 @@ def ultrasonic_go_to_position(s, front, left, right, unit='inch', p=.1):
                 dir = dir_neg
             if speed > 1.0:
                 speed = 1.0
-            s.move_pid(speed, dir, 0)
+            if speed != last_speed:
+                s.move_pid(speed, dir, 0)
+                last_speed = speed
         s.stop()
 
     #TODO: update speed based on distance once values have been tuned in the the one sensor version
