3a294d6ff7ad050d2a941efcaf7a741643339423
Andrew <amessing@utk.edu>
2015-11-19 01:08:14 +0000

    Fixed ultrasonic stuff

--- utils/lineup_test.py
+++ utils/lineup_test.py
@@ -10,16 +10,17 @@ from head.spine.voltage import get_battery_voltage
 from head.spine.Vec3d import Vec3d
 from head.spine.loader import Loader
 from head.spine.control import keyframe
-
+from head.spine.ultrasonic import strafe_at_distance
+from head.spine.ultrasonic import ultrasonic_go_to_position
+from head.spine.control import trapezoid
 fmt = '%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=fmt, level=logging.WARNING, datefmt='%I:%M:%S')
 # logging.basicConfig(format=fmt, level=logging.DEBUG, datefmt='%I:%M:%S')
 logger = logging.getLogger(__name__)
 
 with get_spine() as s:
-    s.move_pid(1,-85,0)
-    ir = s.read_ir_a()['left']
-    while ir < 600:
-        print ir
-        ir = s.read_ir_a()['left']
+    trapezoid(s.move_pid, (0, 0, 0), (1, 0, 0), (1, 0, 0), 3.1)
+    ultrasonic_go_to_position(s, 7,  float('inf'), float('inf'), 'cm',.2)
+    s.move_pid(.75, 45, 0)
+    time.sleep(.3)
     s.stop()
